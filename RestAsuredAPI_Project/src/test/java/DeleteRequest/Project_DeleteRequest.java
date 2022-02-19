5fa0664db1258e312e07cff8b17f9c93df4cd6af
GAadarsh7 <aadarsh.g@collabera.com>
2022-02-19 17:03:56 +0530

    Capstone Project - RestAssured

---
+++ RestAsuredAPI_Project/src/test/java/DeleteRequest/Project_DeleteRequest.java
@@ -0,0 +1,27 @@
+package DeleteRequest;
+
+import org.json.JSONObject;
+import org.testng.annotations.Test;
+
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+
+public class Project_DeleteRequest 
+{
+	@Test
+	public void Delete_Request() 
+	{
+		
+		
+		  Response resp=RestAssured
+		.given()
+		.delete("https://reqres.in/api/users ");
+		 
+		  System.out.println("delete Response body is:"+resp.asString());
+		  System.out.println("the status response code is :"+resp.getStatusCode());
+
+
+	}
+
+}
